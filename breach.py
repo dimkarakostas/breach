32bc9fdedb46763e69815f8f165a21096a59a6fd
dimkarakostas <dimit.karakostas@gmail.com>
2015-03-14 17:48:23 +0200

    Update

--- breach.py
+++ breach.py
@@ -4,6 +4,7 @@ import logging
 import binascii
 
 #TLS Header
+TLS_HEADER_LENGTH = 5
 TLS_CONTENT_TYPE = 0
 TLS_VERSION_MAJOR = 1
 TLS_VERSION_MINOR = 2
@@ -16,7 +17,8 @@ TLS_ALERT = 21
 TLS_HANDSHAKE = 22
 TLS_APPLICATION_DATA = 23
 TLS_HEARTBEAT = 24
-TLS_CONTENT = {TLS_CHANGE_CIPHER_SPEC: "Change cipher spec", TLS_ALERT: "Alert", TLS_HANDSHAKE: "Handshake", TLS_APPLICATION_DATA: "Application Data", TLS_HEARTBEAT: "Heartbeat"}
+TLS_CONTENT = {TLS_CHANGE_CIPHER_SPEC: "Change cipher spec (20)", TLS_ALERT: "Alert (21)", TLS_HANDSHAKE: "Handshake (22)", TLS_APPLICATION_DATA: "Application Data (23)", TLS_HEARTBEAT: "Heartbeat (24)"}
+TLS_VERSION = {(3, 0) : "SSL 3.0", (3, 1): "TLS 1.0", (3, 2): "TLS 1.1", (3, 3): "TLS 1.2"}
 
 #Ports and hosts
 USER = "" #listen requests from everyone
@@ -31,7 +33,6 @@ data_buff = 4096
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-
 
 def log_data(data, b=16):
     pad = 0
@@ -51,28 +52,37 @@ def log_data(data, b=16):
     return "\n".join(output)
 
 def parse(data, response=False):
-    lg = []
+    lg = ["\n"]
     cont_type = ord(data[TLS_CONTENT_TYPE])
+    version = (ord(data[TLS_VERSION_MAJOR]), ord(data[TLS_VERSION_MINOR]))
     length = 256*ord(data[TLS_LENGTH_MAJOR]) + ord(data[TLS_LENGTH_MINOR])
     if (response):
-            print("Endpoint : %d bytes" % length)
-            lg.append("\nEndpoint : %d bytes" % length)
+            if (cont_type == 23):
+                    print("Endpoint : %d" % len(data))
+            lg.append("Source : Endpoint")
     else:
-            print("User : %d" % length)
-            lg.append("\nUser : %d" % length)
-    if cont_type in TLS_CONTENT:
-        lg.append("Content is : " + TLS_CONTENT[cont_type])
-    else:
-        lg.append("Unassigned content type: %d" % cont_type)
-    lg.append(log_data(data))
+            if (cont_type == 23):
+                    print("User : %d" % len(data))
+            lg.append("Source : User")
+    try:
+        lg.append("Content Type : " + TLS_CONTENT[cont_type])
+    except:
+        lg.append("Content Type: Unassigned %d" % cont_type)
+    try:
+        lg.append("TLS Version : " + TLS_VERSION[(version[0], version[1])])
+    except:
+        lg.append("TLS Version: Uknown %d %d" % (version[0], version[1]))
+    lg.append("Payload Length: %d\n" % length)
+    lg.append(log_data(data[TLS_HEADER_LENGTH:TLS_HEADER_LENGTH+length]))
+    lg.append("\n")
     logger.debug("\n".join(lg))
 
 def start():
     #start sockets on user side (proxy as server) and endpoint side (proxy as client)
-    logger.debug("Starting Proxy")
+    logger.info("Starting Proxy")
     user_setup()
     endpoint_setup()
-    logger.debug("Proxy is set up")
+    logger.info("Proxy is set up")
 
 def restart():
     #restart sockets in case of problem
@@ -83,33 +93,33 @@ def restart():
         pass
     user_setup()
     endpoint_setup()
-    logger.debug("Proxy has restarted")
+    logger.info("Proxy has restarted")
 
 def user_setup():
     global user_socket, user_connection, address
-    logger.debug("Setting up user socket")
+    logger.info("Setting up user socket")
     user_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     user_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #reuse socket option
     user_socket.bind((USER, USER_PORT))
-    logger.debug("User bind complete")
+    logger.info("User bind complete")
 #    user_socket.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1) #include IP headers
     user_socket.listen(1)
-    logger.debug("User listen complete")
+    logger.info("User listen complete")
     user_connection, address = user_socket.accept()
-    logger.debug("User socket is set up")
+    logger.info("User socket is set up")
 
 def endpoint_setup():
     global endpoint_socket
-    logger.debug("Setting up endpoint socket")
+    logger.info("Setting up endpoint socket")
     endpoint_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    logger.debug("Connecting endpoint socket")
+    logger.info("Connecting endpoint socket")
     endpoint_socket.connect((ENDPOINT, ENDPOINT_PORT))
     endpoint_socket.setblocking(0) #set non-blocking, i.e. raise exception if send/recv is not completed
-    logger.debug("Endpoint socket is set up")
+    logger.info("Endpoint socket is set up")
 
 #proxy loop
 start()
-logger.debug("starting main loop")
+logger.info("Starting main loop")
 while 1:
     ready_to_read, ready_to_write, in_error = select.select([user_connection, endpoint_socket], [], [], 10)
     
@@ -118,19 +128,19 @@ while 1:
             try:
                 data = user_connection.recv(data_buff)
             except:
-                logger.debug("User connection error")
-                logger.debug(type(exc) + "\n" + exc.args + "\n" + exc)
+                logger.error("User connection error")
+                logger.error(type(exc) + "\n" + exc.args + "\n" + exc)
                 restart()
             if (len(data)==0):
-                    logger.debug("User connection closed")
+                    logger.info("User connection closed")
                     restart()
             else:
                     parse(data)
                     try:
                         endpoint_socket.sendall(data)
                     except:
-                        logger.debug("User data forwarding error")
-                        logger.debug(type(exc) + "\n" + exc.args + "\n" + exc)
+                        logger.error("User data forwarding error")
+                        logger.error(type(exc) + "\n" + exc.args + "\n" + exc)
                         restart()
 
     if endpoint_socket in ready_to_read:
@@ -138,21 +148,21 @@ while 1:
             try:
                 data = endpoint_socket.recv(data_buff)
             except Exception as exc:
-                logger.debug("Endpoint connection error")
-                logger.debug(type(exc) + "\n" + exc.args + "\n" + exc)
+                logger.error("Endpoint connection error")
+                logger.error(type(exc) + "\n" + exc.args + "\n" + exc)
                 restart()
             if (len(data)==0):
-                    logger.debug("Endpoint connection closed")
+                    logger.info("Endpoint connection closed")
                     restart()
             else:
                     parse(data, True)
                     try:
                         user_connection.sendall(data)
                     except:
-                        logger.debug("Endpoint data forwarding error")
-                        logger.debug(type(exc) + "\n" + exc.args + "\n" + exc)
+                        logger.error("Endpoint data forwarding error")
+                        logger.error(type(exc) + "\n" + exc.args + "\n" + exc)
                         restart()
 
 user_connection.close()
 endpoint_socket.close()
-logger.debug("Connection closed")
+logger.info("Connection closed")

